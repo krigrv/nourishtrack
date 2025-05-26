455eb021af888f478ec4c2a26fbdd5662eac6a81
Krishna Gaurav <kri.grv@gmail.com>
2025-05-27 04:00:16 +0530

    v2.2c

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
+  serverExternalPackages: [],
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src')
+    };
     return config;
   },
 };
